ad1c144dea21e2ceb2fcb1b99f3cb136ba6d5198
Oleg Doronin <dorooleg@yandex.ru>
2018-03-29 17:15:01 +0300

    snapshots with functor

--- example/snapshot_example/snapshot_example.cpp
+++ example/snapshot_example/snapshot_example.cpp
@@ -4,6 +4,13 @@
 #include <cstdlib>
 #include <vector>
 
+struct ValueHolder
+{
+    ValueHolder(int& v) : v(v) { }
+    void operator()() { ++v; }
+    int& v;
+};
+
 int main()
 {
     rl::snapshot_master master(5002);
@@ -20,15 +27,48 @@ int main()
         }
 
         snapshots.back().stop();
-        snapshots.resize(1);
         snapshots.front().start();
+        snapshots.clear();
+
+        snapshots.push_back(master.wait_snapshot());
+        while (!snapshots.back().is_finish())
+        {
+            snapshots.push_back(master.wait_snapshot());
+        }
+
+        for (std::vector<rl::snapshot>::iterator it = snapshots.begin(); it != snapshots.end(); ++it)
+        {
+            std::cout << it->get_id() << " ";
+        }
+
+        std::cout << std::endl;
+
+        snapshots.back().stop();
+        snapshots.front().start();
+        snapshots.clear();
+
+        snapshots.push_back(master.wait_snapshot());
+        while (!snapshots.back().is_finish())
+        {
+            snapshots.push_back(master.wait_snapshot());
+        }
+
+        for (std::vector<rl::snapshot>::iterator it = snapshots.begin(); it != snapshots.end(); ++it)
+        {
+            std::cout << it->get_id() << " ";
+        }
+
+        std::cout << std::endl;
+
 
         exit(0);
     }
 
-    rl::snapshot_slave::make_snapshot(5002, 1);
+    int i = 5;
 
-    std::cout << "Slave" << std::endl;
+    rl::snapshot_slave::make_snapshot(5002, 1, ValueHolder(i));
+
+    std::cout << "Slave " << i << std::endl;
 
     rl::snapshot_slave::make_snapshot(5002, 0, true);
 }
--- relacy/snapshots/snapshot_slave.hpp
+++ relacy/snapshots/snapshot_slave.hpp
@@ -12,37 +12,60 @@
 namespace rl
 {
 
+
 class snapshot_slave
 {
+private:
+    class empty_functor
+    {
+    public:
+        void operator()() { }
+    };
+
 public:
     static void make_snapshot(unsigned int port, uint64_t id, bool finish = false)
     {
-        pid_t pid = fork();
+        make_snapshot(port, id, empty_functor(), finish);
+    }
 
-        if (pid != 0)
+    template<typename T>
+    static void make_snapshot(unsigned int port, uint64_t id, T functor, bool finish = false)
+    {
+        while (true)
         {
-            int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-            struct hostent *server = gethostbyname("localhost");
-            struct sockaddr_in server_addr = {};
-            server_addr.sin_family = AF_INET;
-            bcopy((char *)server->h_addr, (char *)&server_addr.sin_addr.s_addr, server->h_length);
-            server_addr.sin_port = htons(port);
+            pid_t pid = fork();
 
-            if (connect(socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+            if (pid != 0)
             {
-                exit(0);
+                int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+                struct hostent *server = gethostbyname("localhost");
+                struct sockaddr_in server_addr = {};
+                server_addr.sin_family = AF_INET;
+                bcopy((char *)server->h_addr, (char *)&server_addr.sin_addr.s_addr, server->h_length);
+                server_addr.sin_port = htons(port);
+
+                if (connect(socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+                {
+                    exit(0);
+                }
+
+                strong_write(socket_fd, reinterpret_cast<char*>(&id), sizeof(id));
+                strong_write(socket_fd, reinterpret_cast<char*>(&pid), sizeof(pid));
+                strong_write(socket_fd, reinterpret_cast<char*>(&finish), sizeof(finish));
+
+                snapshot_event event = snapshot_event_start;
+                strong_read(socket_fd, reinterpret_cast<char*>(&event), sizeof(event));
+
+                if (event == snapshot_event_stop)
+                {
+                    exit(0);
+                }
+
+                functor();
             }
-
-            strong_write(socket_fd, reinterpret_cast<char*>(&id), sizeof(id));
-            strong_write(socket_fd, reinterpret_cast<char*>(&pid), sizeof(pid));
-            strong_write(socket_fd, reinterpret_cast<char*>(&finish), sizeof(finish));
-
-            snapshot_event event = snapshot_event_start;
-            strong_read(socket_fd, reinterpret_cast<char*>(&event), sizeof(event));
-
-            if (event == snapshot_event_stop)
+            else
             {
-                exit(0);
+                break;
             }
         }
     }
